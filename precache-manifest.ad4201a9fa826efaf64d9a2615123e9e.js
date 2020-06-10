36a3222ba2477fefa9f09cf9c9a50695bb462fe5
mami tsuchiya <mami_tsuchiya@beeworks.co,jp>
2020-06-10 17:33:22 +0900

    add

---
+++ precache-manifest.ad4201a9fa826efaf64d9a2615123e9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66577d5d2d611dac12c2",
+    "url": "/css/app.ba780763.css"
+  },
+  {
+    "revision": "ad48b13e2599f7cc4f6d",
+    "url": "/css/chunk-vendors.f4dc5591.css"
+  },
+  {
+    "revision": "6a052fe45e350366e0ac3204f130d847",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66577d5d2d611dac12c2",
+    "url": "/js/app.5a10a318.js"
+  },
+  {
+    "revision": "ad48b13e2599f7cc4f6d",
+    "url": "/js/chunk-vendors.878de0af.js"
+  },
+  {
+    "revision": "815fbf42409708922cc1901012dc3e44",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "6c4e407c3a02a9376884f4b73f6a6936",
+    "url": "/robots.txt"
+  }
+]);
